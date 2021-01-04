ab31a8f7eef2484d8216d26b71180ba3c1b5f010
jihoBae <jiho@dgu.ac.kr>
2021-01-05 01:14:14 +0900

    change the time of the status bar to the real time using JS..

---
+++ javascripts/clock.js
@@ -0,0 +1,19 @@
+const clockBar = document.querySelector(".js-clock-bar"),
+  clockNow = clockBar.querySelector(".js-clock-now");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockNow.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
